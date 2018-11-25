d5d0662ad12feb876695d29eed24d49d0a84da9c
Your Name <you@example.com>
2018-11-25 21:19:57 +0200

    UI

--- app/src/main/java/com/example/android/corsi/MainActivity.java
+++ app/src/main/java/com/example/android/corsi/MainActivity.java
@@ -23,8 +23,11 @@ public class MainActivity extends AppCompatActivity {
                 case R.id.navigation_dashboard:
                     mTextMessage.setText(R.string.title_dashboard);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_downloads:
+                    mTextMessage.setText(R.string.title_downloads);
+                    return true;
+                case R.id.navigation_settings:
+                    mTextMessage.setText(R.string.title_settings);
                     return true;
             }
             return false;
